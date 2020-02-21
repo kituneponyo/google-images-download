229c8f577cb8b997cf93d0149a7ae76402b44bba
kituneponyo <kaoru-izutani@e-mail.jp>
2020-02-21 11:48:00 +0900

    debug
    I saw that variable object_raw is yet utf-8 encoded and unicode_decoded

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -749,8 +749,7 @@ class googleimagesdownload:
         start_object = s.find('[', start_line + 1)
         end_object = s.find('</script>', start_object + 1) - 4
         object_raw = str(s[start_object:end_object])
-        object_decode = bytes(object_raw, "utf-8").decode("unicode_escape")
-        image_objects = json.loads(object_decode)[31][0][12][2]
+        image_objects = json.loads(object_raw)[31][0][12][2]
         image_objects = [x for x in image_objects if x[0]==1]
         return image_objects
 
